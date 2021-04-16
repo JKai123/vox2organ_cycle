29c142aaa12cf1053713429eb9d0d602c9ad1c4c
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-04-16 19:10:41 +0200

    Add mesh losses

---
+++ src/utils/losses.py
@@ -0,0 +1,114 @@
+
+""" Loss function collection for convenient calculation over multiple classes
+and/or instances of meshes.
+
+    Notation:
+        - B: batch size
+        - I: number of instances, e.g. positions where the loss is computed in the
+        - C: number of channels (= number of classes usually)
+        architecture
+        - V: number of vertices
+        - F: number of faces
+"""
+
+__author__ = "Fabi Bongratz"
+__email__ = "fabi.bongratz@gmail.com"
+
+from abc import ABC, abstractmethod
+
+import torch
+import pytorch3d.structures
+from pytorch3d.loss import (chamfer_distance,
+                            mesh_edge_loss,
+                            mesh_laplacian_smoothing,
+                            mesh_normal_consistency)
+from pytorch3d.ops import sample_points_from_meshes
+
+class MeshLoss(ABC):
+    def __call__(self, pred_meshes, target):
+        """ Chamfer loss calculation
+
+        :param pred_meshes: A multidimensional array of predicted meshes of shape
+        (I, C), each of type pytorch3d.structures.Meshes
+        :param target: A multidimensional array of target points of shape (I, C),
+        each of type pytorch3d.structures.Pointclouds
+        :return: The calculated loss.
+        """
+        mesh_loss = 0.
+
+        I = pred_meshes.shape[0]
+        C = pred_meshes.shape[1]
+
+        for i in range(I):
+            for c in range(C-1):
+                mesh_loss += self.get_loss(pred_meshes[i][c], target[i,c])
+
+        return mesh_loss
+
+    @abstractmethod
+    def get_loss(self,
+                 pred_meshes: pytorch3d.structures.Meshes,
+                 target: pytorch3d.structures.Pointclouds):
+        pass
+
+class ChamferLoss(MeshLoss):
+    def get_loss(self,
+                 pred_meshes: pytorch3d.structures.Meshes,
+                 target: pytorch3d.structures.Pointclouds):
+        n_points = torch.min(target.num_points_per_cloud())
+        pred_points = sample_points_from_meshes(pred_meshes, n_points)
+        return chamfer_distance(pred_points, target)[0]
+
+class LaplacianLoss(MeshLoss):
+    def get_loss(self,
+                 pred_meshes: pytorch3d.structures.Meshes,
+                 target: pytorch3d.structures.Pointclouds=None):
+        return mesh_laplacian_smoothing(pred_meshes, method='uniform')
+
+class NormalConsistencyLoss(MeshLoss):
+    def get_loss(self,
+                 pred_meshes: pytorch3d.structures.Meshes,
+                 target: pytorch3d.structures.Pointclouds=None):
+        return mesh_normal_consistency(pred_meshes)
+
+class EdgeLoss(MeshLoss):
+    def get_loss(self,
+                 pred_meshes: pytorch3d.structures.Meshes,
+                 target: pytorch3d.structures.Pointclouds=None):
+        return mesh_edge_loss(pred_meshes)
+
+def linear_loss_combine(losses, weights):
+    """ Compute the losses in a linear manner, e.g.
+    a1 * loss1 + a2 * loss2 + ...
+
+    :param losses: The individual losses.
+    :param weights: The weights for the losses.
+    :returns: The overall (weighted) loss.
+    """
+    loss_total = 0
+    for loss, weight in zip(losses, weights):
+        loss_total += weight * loss
+
+        # wandb.log({loss_func.__name__: loss})
+
+    # wandb.log({'loss_total': loss_total})
+
+    return loss_total
+
+def geometric_loss_combine(losses, weights):
+    """ Compute the losses in a geometric manner, e.g.
+    (loss1^a1) * (loss2^a2) * ...
+
+    :param losses: The individual losses.
+    :param weights: The weights for the losses.
+    :returns: The overall (weighted) loss.
+    """
+    loss_total = 1
+    for loss, weight in zip(losses, weights):
+        loss_total *= torch.pow(loss, weight)
+
+        # wandb.log({loss_func.__name__: loss})
+
+    # wandb.log({'loss_total': loss_total})
+
+    return loss_total
