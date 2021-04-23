99b7ec8dfb97bf49ca42760313f0c4d8b389e7dc
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-04-23 10:32:16 +0200

    Add mesh to preprocess check

--- src/check/check_preprocess.py
+++ src/check/check_preprocess.py
@@ -27,22 +27,26 @@ def run_preprocess_check():
     training_set,\
             _,\
             _ = dataset_split_handler['Hippocampus'](augment_train=False,
+                                                     save_dir="../misc",
                                                              **hps_lower)
     # Augmentation
     training_set_augment,\
             _,\
             _ = dataset_split_handler['Hippocampus'](augment_train=True,
+                                                     save_dir="../misc",
                                                              **hps_lower)
 
-    for iter_in_epoch, (data, data_augment) in tqdm(enumerate(zip(training_set[:5],
-                                                  training_set_augment[:5])),
-                                                  desc="Testing...",
-                                                  position=0,
-                                                  leave=True):
+    for iter_in_epoch in tqdm(range(5), desc="Testing...", position=0, leave=True):
+        data = training_set.get_item_and_mesh_from_index(iter_in_epoch)
+        data_augment = training_set_augment.get_item_and_mesh_from_index(iter_in_epoch)
         img_slices = [data[0][32, :, :], data[0][:, 32, :], data[0][:, :, 32]]
         label_slices = [data[1][32, :, :], data[1][:, 32, :], data[1][:, :, 32]]
+        mesh = data[2]
+        mesh.store("../misc/mesh" + str(iter_in_epoch) + ".ply")
         img_slices_augment = [data_augment[0][32, :, :], data_augment[0][:, 32, :], data_augment[0][:, :, 32]]
         label_slices_augment = [data_augment[1][32, :, :], data_augment[1][:, 32, :], data_augment[1][:, :, 32]]
+        mesh_augment = data_augment[2]
+        mesh_augment.store("../misc/mesh" + str(iter_in_epoch) + "_augment.ply")
         show_slices(img_slices, label_slices, "../misc/img" +\
                     str(iter_in_epoch) + ".png")
         show_slices(img_slices, None, "../misc/img" +\
