8e5d5218f54ee4916707318daa6426e17268d309
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-05-27 15:57:53 +0200

    Add class MeshesOfMeshes

--- src/utils/mesh.py
+++ src/utils/mesh.py
@@ -6,6 +6,7 @@ __email__ = "fabi.bongratz@gmail.com"
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 from trimesh import Trimesh
 from pytorch3d.structures import Meshes
 from pytorch3d.utils import ico_sphere
@@ -81,23 +82,91 @@ class Mesh():
 
         return vox_occupied
 
-class MeshCollection():
-    """ Collection of meshes with shape (S,C)
+class MeshesOfMeshes():
+    """ Extending pytorch3d.structures.Meshes so that each mesh in a batch of
+    meshes can consist of several distinguishable meshes. Basically, a new
+    dimension 'M' is introduced to tensors of vertices and faces.
+
+    Shapes of self.faces (analoguously for vertices and normals):
+        - padded (N,M,F,3)
+        - packed (N*M*F,3)
+    where N is the batch size, M is the number of meshes per sample, and F
+    is the number of faces per connected mesh. In general, M and F can be
+    different for every mesh and their maximum is used in the padded
+    representation.
     """
-    def __init__(self, n_steps, n_classes, vertices, faces):
-        self.n_steps = n_steps # S
-        self.n_classes = n_classes # C
+    def __init__(self, verts, faces):
+        self._Meshes_list = []
+        for v, f in zip(verts, faces):
+            self._Meshes_list.append(Meshes(v,f))
 
-        self.vertices = vertices
-        self.faces = faces
+        self._verts_packed = None
+        self._faces_packed = None
+        self._verts_normals_packed = None
+        self._edges_packed = None
 
-    def __getitem__(self, index):
-        if len(index) != 2:
-            raise ValueError("Index should define two-dimensional"\
-                             " coordinates but is of length {len(index)}.")
-        s, c = index
+        self.N = self.get_N()
+        self.M, self.V, self.F = self.get_M_V_F()
 
-        return Mesh(self.vertices[s][c], self.faces[s,c])
+    def verts_list(self):
+        return [m.verts_list() for m in self._Meshes_list]
+
+    def get_N(self):
+        return len(self._Meshes_list)
+
+    def get_M_V_F(self):
+        max_M = 0
+        max_V = 0
+        max_F = 0
+        for m in self._Meshes_list:
+            if m.verts_padded().shape[0] > max_M:
+                max_M = m.verts_padded().shape[0]
+            if m.verts_padded().shape[1] > max_V:
+                max_V = m.verts_padded().shape[1]
+            if m.faces_padded().shape[1] > max_F:
+                max_F = m.faces_padded().shape[1]
+
+        return max_M, max_V, max_F
+
+    def verts_packed(self):
+        if self._verts_packed is None:
+            self._verts_packed = torch.cat(
+                [m.verts_packed() for m in self._Meshes_list], dim=0
+            )
+
+        return self._verts_packed
+
+    def verts_normals_packed(self):
+        if self._verts_normals_packed is None:
+            self._verts_normals_packed = torch.cat(
+                [m.verts_normals_packed() for m in self._Meshes_list], dim=0
+            )
+
+        return self._verts_normals_packed
+
+    def faces_packed(self):
+        V_prev = 0
+        if self._faces_packed is None:
+            faces_packed = []
+            for m in self._Meshes_list:
+                faces_packed.append(m.faces_packed() + V_prev)
+                V_pref += m.verts_packed().shape[0]
+
+            self._faces_packed = torch.cat(faces_packed, dim=0)
+
+        return self._faces_packed
+
+    def edges_packed(self):
+        V_prev = 0
+        if self._edges_packed is None:
+            edges_packed = []
+            for m in self._Meshes_list:
+                edges_packed.append(m.edges_packed() + V_prev)
+                V_pref += m.verts_packed().shape[0]
+
+            self._edges_packed = torch.cat(edges_packed, dim=0)
+
+        return self._edges_packed
 
 def verts_faces_to_Meshes(verts, faces, ndim):
     """ Convert lists of vertices and faces to lists of
