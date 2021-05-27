fb5e0a28fe242aa1559bb8133562b2caf53bc59d
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-05-27 16:34:27 +0200

    Add script for template creation

---
+++ src/scripts/create_template_and_store.py
@@ -0,0 +1,19 @@
+
+""" Create a mesh template and store it """
+
+__author__ = "Fabi Bongratz"
+__email__ = "fabi.bongratz@gmail.com"
+
+from data.cortex import Cortex
+
+template_path = "../supplementary_material/spheres/cortex_white_matter_spheres_40962.obj"
+
+dataset, _, _ = Cortex.split("/mnt/nas/Data_Neuro/MALC_CSR/",
+                             0,
+                             (100, 0, 0),
+                             (192, 224, 192),
+                             False,
+                             "../misc")
+
+dataset.store_template(template_path)
+
