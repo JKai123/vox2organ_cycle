26c529c30514ad3f04bc8d6f6370dfb1a28f6bd7
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-06-01 17:41:55 +0200

    Adapt script to previous changes in dataset

--- src/scripts/create_template_and_store.py
+++ src/scripts/create_template_and_store.py
@@ -6,15 +6,20 @@ __email__ = "fabi.bongratz@gmail.com"
 
 from data.cortex import Cortex
 
-template_path = "../supplementary_material/spheres/cortex_convex.obj"
+structure_type = 'white_matter'
+
+template_path = "../supplementary_material/spheres/cortex_" + structure_type + "_convex.obj"
 
 dataset, _, _ = Cortex.split("/mnt/nas/Data_Neuro/MALC_CSR/",
                              1532,
                              (100, 0, 0),
-                             (192, 224, 192),
                              False,
-                             "../misc")
+                             "../misc",
+                             patch_size=(192, 224, 192),
+                             structure_type=structure_type,
+                             mesh_target_type='mesh',
+                             n_ref_points_per_structure = None)
 
-path = dataset.store_convex_cortex_template(template_path, 100000)
+path = dataset.store_convex_cortex_template(template_path, 200000)
 
 print("Template stored at " + path)
