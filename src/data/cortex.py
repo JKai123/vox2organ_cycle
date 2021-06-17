Bugfix: normalization of vertices

src/data/cortex.py
@@ -444,7 +444,8 @@ class Cortex(DatasetHandler):
             vertices = m.vertices.view(self.n_m_classes, -1, 3)
             faces = m.faces.view(self.n_m_classes, -1, 3)
             unnorm_verts = unnormalize_vertices(
-                vertices.view(-1, 3), torch.tensor(self.patch_size)[None]
+                vertices.view(-1, 3),
+                torch.tensor(self.patch_size).flip(dims=[0])[None]
             ).view(self.n_m_classes, -1, 3)
             pv = Mesh(unnorm_verts, faces).get_occupied_voxels(np.flip(
                           self.patch_size, axis=0

src/data/dataset.py
@@ -228,7 +228,7 @@ class DatasetHandler(torch.utils.data.Dataset):
         for i in tqdm(range(len(self)),
                       desc="Checking IoU of voxel and mesh labels"):
             _, voxel_label, mesh = self.get_item_and_mesh_from_index(i)
-            shape = torch.tensor(voxel_label.shape)[None]
+            shape = torch.tensor(voxel_label.shape).flip(dims=[0])[None]
             vertices, faces = mesh.vertices, mesh.faces
             voxelized_mesh = torch.zeros_like(voxel_label, dtype=torch.long)
             vertices = vertices.view(self.n_m_classes, -1, 3)
@@ -236,9 +236,9 @@ class DatasetHandler(torch.utils.data.Dataset):
             unnorm_verts = unnormalize_vertices(
                 vertices.view(-1, 3), shape
             ).view(self.n_m_classes, -1, 3)
-            pv = Mesh(unnorm_verts, faces).get_occupied_voxels(np.flip(
-                          shape.squeeze().cpu().numpy(), axis=0
-            ))
+            pv = Mesh(unnorm_verts, faces).get_occupied_voxels(
+                shape.squeeze().cpu().numpy()
+            )
             if pv is not None:
                 pv_flip = np.flip(pv, axis=1)  # convert x,y,z -> z, y, x
                 # Occupied voxels are considered to belong to one class

src/utils/eval_metrics.py
@@ -37,17 +37,17 @@ def JaccardMeshScore(pred, data, n_v_classes, n_m_classes, model_class,
     """
     input_img = data[0].cuda()
     voxel_target = data[1].cuda()
-    shape = torch.tensor(voxel_target.shape)[None]
+    shape = torch.tensor(voxel_target.shape).flip(dims=[0])[None]
     vertices, faces = model_class.pred_to_verts_and_faces(pred)
     voxel_pred = torch.zeros_like(voxel_target, dtype=torch.long)
     # Only mesh of last step considered and batch dimension squeezed out
     vertices = vertices[-1].view(n_m_classes, -1, 3)
     faces = faces[-1].view(n_m_classes, -1, 3)
     unnorm_verts = unnormalize_vertices(
-        vertices.view(-1, 3), shape
+        vertices.view(-1, 3), shape.flip(dims=[1])
     ).view(n_m_classes, -1, 3)
     pv = Mesh(unnorm_verts, faces).get_occupied_voxels(
-        np.flip(shape.squeeze().cpu().numpy(), axis=0)
+        shape.squeeze().cpu().numpy()
     )
     if pv is not None:
         pv_flip = np.flip(pv, axis=1)  # convert x,y,z -> z, y, x