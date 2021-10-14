9fdd1f44210830323840d12da55f6fa2ea1a56ad
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-10-14 19:05:25 +0200

    Bugfix in assemble_template.py

--- src/scripts/assemble_template.py
+++ src/scripts/assemble_template.py
@@ -16,9 +16,9 @@ rh_pial = trimesh.load("rh_pial.ply")
 
 scene = Scene()
 
-scene.add(lh_white, geom_name="lh_white")
-scene.add(rh_white, geom_name="rh_white")
-scene.add(lh_pial, geom_name="lh_pial")
-scene.add(rh_pial, geom_name="rh_pial")
+scene.add_geometry(lh_white, geom_name="lh_white")
+scene.add_geometry(rh_white, geom_name="rh_white")
+scene.add_geometry(lh_pial, geom_name="lh_pial")
+scene.add_geometry(rh_pial, geom_name="rh_pial")
 
-scene.export("PLACE_FILE_NAME_HERE.obj")
+scene.export("INSERT_TEMPLATE_NAME_HERE")
