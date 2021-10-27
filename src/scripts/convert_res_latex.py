27488bebb9e7277061ca98d4cff0693f3b7d2670
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-10-27 17:56:11 +0200

    Add script to convert ad_hd results to latex table entry

---
+++ src/scripts/convert_res_latex.py
@@ -0,0 +1,51 @@
+
+""" Convert result files to latex table entry. """
+
+__author__ = "Fabi Bongratz"
+__email__ = "fabi.bongratz@gmail.com"
+
+import os
+import csv
+from argparse import ArgumentParser
+
+FILE_NAMES = ("lh_white_eval_ad_hd_summary.csv",
+              "rh_white_eval_ad_hd_summary.csv",
+              "lh_pial_eval_ad_hd_summary.csv",
+              "rh_pial_eval_ad_hd_summary.csv",
+              "eval_ad_hd_summary.csv")
+
+if __name__ == '__main__':
+    argparser = ArgumentParser(description="Mesh evaluation procedure")
+    argparser.add_argument('exp_name',
+                           type=str,
+                           help="Name of experiment under evaluation.")
+    argparser.add_argument('n_test_vertices',
+                           type=int,
+                           help="The number of template vertices for each"
+                           " structure that was used during testing.")
+
+    args = argparser.parse_args()
+    exp_name = args.exp_name
+    n_test_vertices = args.n_test_vertices
+
+    exp_dir = f"../experiments/{exp_name}/test_template_{n_test_vertices}"
+
+    out_file = os.path.join(exp_dir, "latex_out.txt")
+    out_str = ""
+
+    for f in FILE_NAMES:
+        f_full = os.path.join(exp_dir, f)
+
+        with open(f_full, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=";")
+            # Read second row
+            r1 = csv_reader.__next__()
+            r2 = csv_reader.__next__()
+            r2 = [float(i) for i in r2]
+            out_str += f"{r2[0]:.3f} \\interval{{{r2[1]:.3f}}} & {r2[2]:.3f} \\interval{{{r2[3]:.3f}}}"
+            if f != FILE_NAMES[-1]:
+                out_str += " & "
+
+    out_str += " \\\\ \n "
+    with open(out_file, 'w') as f_out:
+        f_out.write(out_str)
