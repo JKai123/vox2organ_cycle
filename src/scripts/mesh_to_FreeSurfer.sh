a1decd79aa6a4f50fd850fbe427d116710c49384
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-11-12 16:55:46 +0100

    Implement mapping from our thickness to fsaverage sphere

---
+++ src/scripts/mesh_to_FreeSurfer.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Transform any input mesh (typically .ply) to FreeSurfer space equal to
+# lh.white etc.
+
+IN_MESH=$1
+ORIG_MGZ=$2
+TRANS_AFFINE_FILE=$3
+OUT_MESH=$4
+
+# Temporary folder to store files needed for the transformation
+TRANS_FOLDER="tmp_trans"
+mkdir -p $TRANS_FOLDER
+
+export IN_MESH
+export TRANS_AFFINE_FILE
+export OUT_MESH
+
+# Get trk --> scanner matrices as described in
+# https://surfer.nmr.mgh.harvard.edu/fswiki/CoordinateSystems,
+# Example 3
+
+# norig
+norig_file="$TRANS_FOLDER/norig.txt"
+if test -f "$norig_file"; then
+    rm $norig_file
+fi
+mri_info --vox2ras "$ORIG_MGZ" >> $norig_file
+# norig inverse
+norig_inv_file="$TRANS_FOLDER/norig_inv.txt"
+if test -f "$norig_inv_file"; then
+    rm $norig_inv_file
+fi
+python -c "import numpy as np;\
+    mat = np.loadtxt('$norig_file');\
+    mat_inv = np.linalg.inv(mat);\
+    np.savetxt('$norig_inv_file', mat_inv)"
+export norig_inv_file
+
+# torig
+torig_file="$TRANS_FOLDER/torig.txt"
+if test -f "$torig_file"; then
+    rm $torig_file
+fi
+mri_info --vox2ras-tkr "$ORIG_MGZ" >> $torig_file
+export torig_file
+
+# Transform mesh
+echo "Processing $IN_MESH"
+
+python - << EOF
+import os
+import sys
+import trimesh
+import numpy as np
+import nibabel as nib
+
+trans_affine = np.loadtxt(os.environ['TRANS_AFFINE_FILE'])
+norig_inv = np.loadtxt(os.environ['norig_inv_file'])
+torig = np.loadtxt(os.environ['torig_file'])
+
+mesh = trimesh.load(os.environ['IN_MESH'])
+verts = mesh.vertices
+verts_affine = np.concatenate(
+    (verts.T, np.ones((1, verts.shape[0]))),
+    axis=0
+)
+
+# Transform
+verts_affine= torig @ norig_inv @ trans_affine @ verts_affine
+
+new_verts = verts_affine.T[:, :-1]
+
+nib.freesurfer.io.write_geometry(os.environ['OUT_MESH'], new_verts, mesh.faces)
+
+sys.exit(0)
+EOF
+
+if [ $? -eq 0 ]; then
+    echo "Finished transformation."
+else
+    echo "Errors occured during transformation."
+fi
+exit 0
---
+++ src/scripts/values_to_fsaverage.py
@@ -0,0 +1,60 @@
+
+""" Map values on a sphere to the fsaverage sphere. """
+
+__author__ = "Fabi Bongratz"
+__email__ = "fabi.bongratz@gmail.com"
+
+import os
+from argparse import ArgumentParser
+
+import numpy as np
+import torch
+import nibabel as nib
+from pytorch3d.ops import knn_points
+
+device = "cuda:1"
+
+argparser = ArgumentParser()
+argparser.add_argument('hemisphere',
+                       type=str,
+                       help="The hemisphere, 'lh' or 'rh'.")
+argparser.add_argument('moving',
+                       type=str,
+                       help="Moving mesh.")
+argparser.add_argument('values',
+                       type=str,
+                       help="The values corresponding to fixed vertices.")
+argparser.add_argument('out',
+                       type=str,
+                       help="Out file for transferred values on fsaverage.")
+
+args = argparser.parse_args()
+moving = nib.freesurfer.io.read_geometry(args.moving)
+moving_values = np.load(args.values)
+
+average_sphere = nib.freesurfer.io.read_geometry(os.path.join(
+    os.environ['FREESURFER_HOME'],
+    "subjects",
+    "fsaverage",
+    "surf",
+    f"{args.hemisphere}.sphere.reg.avg"
+))
+
+print(f"Transferring values from {args.moving} to fsaverage sphere of"
+      f" {args.hemisphere}")
+
+
+moving_verts = torch.tensor(moving[0]).to(device)[None]
+fixed_verts = torch.tensor(average_sphere[0]).to(device)[None]
+
+# Weighted sum of neighboring values
+K = 5
+dists, idx, _ = knn_points(fixed_verts, moving_verts, K=K)
+dists = dists.squeeze()
+weights = dists / dists.sum(axis=1, keepdim=True)
+moving_values = torch.tensor(moving_values).to(device)
+moved_values = (moving_values[idx.squeeze()] * weights).sum(1).cpu().numpy()
+
+np.save(args.out, moved_values)
+
+print("Wrote values transferred to average sphere to ", args.out)
