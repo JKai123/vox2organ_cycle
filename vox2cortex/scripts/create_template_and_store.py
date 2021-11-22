825bd377fb819d7f994e7ca18fd466607e274253
Fabian Bongratz <fabi.bongratz@gmail.com>
2021-11-22 16:48:31 +0100

    Update script for template creation

--- vox2cortex/scripts/create_template_and_store.py
+++ vox2cortex/scripts/create_template_and_store.py
@@ -11,17 +11,13 @@ structure_type = ('white_matter', 'cerebral_cortex')
 # Important!!! (defines coordinate normalization in the template)
 patch_size = [128, 144, 128]
 select_patch_size = [192, 208, 192]
-patch_origin = [0, 0, 0]
+patch_origin = [0, 0, 0] # Not used here
 n_vertices = 40962
 
-template_path = f"../supplementary_material/white_pial/cortex_4_TYPE_{n_vertices}_sps{select_patch_size}_ps{patch_size}.obj"
+template_path = f"../supplementary_material/new_templates/cortex_4_TYPE_{n_vertices}_sps{select_patch_size}_ps{patch_size}.obj"
 
 split = {
-    'train': ['1000_3', '1001_3', '1002_3', '1006_3', '1007_3', '1008_3',
-              '1009_3', '1010_3', '1011_3', '1012_3', '1013_3', '1014_3',
-              '1015_3', '1036_3', '1017_3', '1107_3', '1128_3', '1101_3',
-              '1003_3', '1004_3', '1125_3', '1005_3', '1122_3', '1110_3',
-              '1119_3', '1113_3', '1018_3', '1019_3', '1104_3', '1116_3'],
+    'train': ['1000_3'], # Use this ID for template creation
     'validation': [],
     'test': []
 }
@@ -30,8 +26,6 @@ print("Creating dataset...")
 dataset, _, _ = Cortex.split(raw_data_dir="/mnt/nas/Data_Neuro/MALC_CSR/",
                              augment_train=False,
                              save_dir="../misc",
-                             dataset_seed=1532,
-                             # dataset_split_proportions=(100, 0, 0),
                              fixed_split=split,
                              patch_origin=patch_origin,
                              select_patch_size=select_patch_size,
@@ -41,14 +35,11 @@ dataset, _, _ = Cortex.split(raw_data_dir="/mnt/nas/Data_Neuro/MALC_CSR/",
                              reduced_freesurfer=0.3,
                              n_ref_points_per_structure=10000, # irrelevant
                              mesh_type='freesurfer',
-                             preprocessed_data_dir="/home/fabianb/data/preprocessed/MALC_CSR/",
                              patch_mode="no")
 print("Dataset created.")
 print("Creating template...")
 
-# path = dataset.store_convex_cortex_template(
-    # template_path, n_min_points=40000, n_max_points=60000
-# )
+# Choose here the function that creates the desired template
 path = dataset.store_ellipsoid_template(template_path, level=6)
 
 if path is not None:
