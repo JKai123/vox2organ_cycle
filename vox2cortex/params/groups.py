cfd5bd1f2aa07cc65cd1083b7b82eeb504f8d6ad
Fabian Bongratz <fabi.bongratz@gmail.com>
2022-02-15 15:18:17 +0100

    Add one hemisphere and remove UNCERTAINTY param

--- vox2cortex/params/groups.py
+++ vox2cortex/params/groups.py
@@ -39,10 +39,6 @@ hyper_ps_groups = {
         'PATCH_SIZE': [128, 144, 128],
         'SELECT_PATCH_SIZE': [192, 208, 192],
         'MODEL_CONFIG': {
-            # Bayesian vox2cortex
-            'UNCERTAINTY': 'mc_10',
-            'P_DROPOUT_UNET': 0.2,
-            'P_DROPOUT_GRAPH': 0.2,
             'GRAPH_CHANNELS': [256, 64, 64, 64, 64],
             'UNPOOL_INDICES': [0,0,0,0],
             'AGGREGATE_INDICES': [
@@ -59,6 +55,46 @@ hyper_ps_groups = {
             'VOXEL_DECODER': True,
             'GC': GraphConvNorm,
             'GROUP_STRUCTS': [[0, 1], [2, 3]],
+        }
+    },
+    # One hemisphere, vox2cortex
+    'one hemisphere': {
+        'MESH_LOSS_FUNC': [
+           ChamferAndNormalsLoss(curv_weight_max=5.0),
+           LaplacianLoss(),
+           NormalConsistencyLoss(),
+           EdgeLoss(0.0)
+        ],
+        'PATCH_MODE': 'single-patch',
+        # Order of structures: lh_white, rh_white, lh_pial, rh_pial; mesh loss
+        # weights should respect this order!
+        'MESH_LOSS_FUNC_WEIGHTS': [
+            [1.0] * 2, # Chamfer
+            [0.01] + [0.0125], # Cosine,
+            [0.1] + [0.25], # Laplace,
+            [0.001] + [0.00225], # NormalConsistency
+            [5.0] * 2 # Edge
+        ],
+        'N_M_CLASSES': 2,
+        'PATCH_SIZE': [64, 144, 128],
+        'SELECT_PATCH_SIZE': [96, 208, 192],
+        'MODEL_CONFIG': {
+            'GRAPH_CHANNELS': [256, 64, 64, 64, 64],
+            'UNPOOL_INDICES': [0,0,0,0],
+            'AGGREGATE_INDICES': [
+                [3,4,5,6],
+                [2,3,6,7],
+                [1,2,7,8],
+                [0,1,7,8] # 8 = last decoder skip
+            ],
+            'NORM': 'batch', # Only for graph convs
+            'DECODER_CHANNELS': [64, 32, 16, 8],
+            'DEEP_SUPERVISION': True,
+            'WEIGHTED_EDGES': False,
+            'PROPAGATE_COORDS': True,
+            'VOXEL_DECODER': True,
+            'GC': GraphConvNorm,
+            'GROUP_STRUCTS': [[0], [1]],
         }
     },
 }
